b444ee1c19a5e857c69400e13b57448606acea60
Koi <koi@Kois-MacBook-Air-3.local>
2024-12-07 18:31:33 -0500

    add home page design

---
+++ sections/Hero/Hero.tsx
@@ -0,0 +1,52 @@
+"use client";
+
+import { motion } from "framer-motion";
+
+function Hero() {
+  return (
+    <div className="flex h-full flex-col items-start justify-center">
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.5, duration: 1 }}
+        className="text-left"
+      >
+        <h1 className="text-6xl font-extrabold leading-tight">
+          Jinlin Wei
+        </h1>
+        <p className="text-4xl font-bold mt-4">
+          Front-end developer based in the north (Valenciennes, Cambrai, Lille)
+        </p>
+      </motion.div>
+      <motion.div
+        initial={{ opacity: 0, y: 10 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{ delay: 0.8, duration: 1 }}
+        className="mt-12 flex flex-col items-start gap-9 sm:flex-row"
+      >
+        {/* Dark Brown Button */}
+        <a href="mailto:weijinlinkoi@gmail.com">
+          <button
+            className="bg-[#4B2E2E] text-white px-6 py-3 rounded-lg text-lg font-semibold hover:bg-[#3E2727] transition"
+          >
+            Get in touch with me
+          </button>
+        </a>
+        {/* Beige Button */}
+        <a
+          href="https://drive.google.com/file/d/1uePShqOvthRCx1tGUZO7px45hvThSxY2/view?usp=drivesdk"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <button
+            className="bg-[#F5F5DC] text-black px-6 py-3 rounded-lg text-lg font-semibold hover:bg-[#EAE0C8] transition"
+          >
+            Download my Resume
+          </button>
+        </a>
+      </motion.div>
+    </div>
+  );
+}
+
+export default Hero;
